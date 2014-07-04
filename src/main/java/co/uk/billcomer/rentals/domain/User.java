d501adcc866dbc85d1a7b69e7d33d53245eeea8a
Bill Comer <bill.comer@gmail.com>
2014-07-04 21:15:36 +0100

    entity for User

--- src/main/java/co/uk/billcomer/rentals/domain/User.java
+++ src/main/java/co/uk/billcomer/rentals/domain/User.java
@@ -3,13 +3,15 @@ package co.uk.billcomer.rentals.domain;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 
-//@Entity
-//@Table(name="USER")
+@Entity
+@Table(name="USER")
 public class User implements Serializable
 {
 
